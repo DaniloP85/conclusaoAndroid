ffe1771ed65a620c5807b54d589eb39788eef497
Flavio Alvarenga <flavio.fgjj@live.com>
2023-04-04 09:55:42 -0300

    navigation + binding + register new user

--- app/src/main/java/br/com/conclusaoandroid/Login.kt
+++ app/src/main/java/br/com/conclusaoandroid/Login.kt
@@ -1,11 +1,28 @@
 package br.com.conclusaoandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.conclusaoandroid.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater);
+
+        val view = binding.root
+        setContentView(view)
+
+        setUpListener()
+    }
+
+    private fun setUpListener() {
+        binding.registerNow.setOnClickListener {
+            val intent = Intent(this, Register::class.java);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/br/com/conclusaoandroid/Register.kt
+++ app/src/main/java/br/com/conclusaoandroid/Register.kt
@@ -1,11 +1,72 @@
 package br.com.conclusaoandroid
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import br.com.conclusaoandroid.databinding.ActivityRegisterBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class Register : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding;
+
+    lateinit var auth: FirebaseAuth;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater);
+
+        val view = binding.root
+        setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
+
+        setUpListener()
     }
+
+    private fun setUpListener() {
+        binding.btnRegister.setOnClickListener {
+            binding.progressBarRegister.visibility = View.VISIBLE;
+            val email = binding.emailRegister.text.toString();
+            val password = binding.passwordRegister.text.toString();
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, getString(R.string.register_validate), Toast.LENGTH_SHORT).show();
+                return@setOnClickListener;
+            }
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    binding.progressBarRegister.visibility = View.GONE;
+
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        //val user = auth.currentUser
+
+                        Toast.makeText(baseContext, "Account created.",
+                            Toast.LENGTH_SHORT).show()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        binding.loginNow.setOnClickListener {
+            val intent = Intent(this, Login::class.java);
+            startActivity(intent);
+            finish();
+        }
+    }
+
 }
