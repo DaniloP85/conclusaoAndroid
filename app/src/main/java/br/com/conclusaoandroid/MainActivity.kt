78d5689c7ff3f36b8f779f66f0365f8ae998183a
Andre Cordeiro <andrecordeiro.oliveira@hotmail.com>
2023-04-05 14:18:54 -0300

    feat: refactor screens login and register

--- app/src/main/java/br/com/conclusaoandroid/MainActivity.kt
+++ app/src/main/java/br/com/conclusaoandroid/MainActivity.kt
@@ -2,14 +2,10 @@ package br.com.conclusaoandroid
 
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import br.com.conclusaoandroid.databinding.ActivityLoginBinding
+import androidx.appcompat.app.AppCompatActivity
 import br.com.conclusaoandroid.databinding.ActivityMainBinding
-import br.com.conclusaoandroid.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding;
@@ -18,10 +14,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
         window.statusBarColor = Color.parseColor("#0075FF")
-
-        binding = ActivityMainBinding.inflate(layoutInflater);
 
         val view = binding.root
         setContentView(view)
