9daa5c87e7378c80944188528fb57f208e58f1a9
Danilo Santos <danilopsnts@gmail.com>
2023-04-11 23:32:58 -0300

    add documentId

--- app/src/main/java/br/com/conclusaoandroid/model/Shopping.kt
+++ app/src/main/java/br/com/conclusaoandroid/model/Shopping.kt
@@ -8,12 +8,6 @@ data class Shopping (
     var date:  Timestamp? = null,
     var marketplace:  String? = null,
     var userId:  String? = null,
+    var documentId:  String? = null,
     var items:  List<ShoppingItem>? = null
-){
-    companion object {
-        const val FIELD_DATE = "date"
-        const val FIELD_MARKPLACE = "marketplace"
-        const val FIELD_ITEMS = "items"
-        const val FIELD_USERID = "userId"
-    }
-}
+)
