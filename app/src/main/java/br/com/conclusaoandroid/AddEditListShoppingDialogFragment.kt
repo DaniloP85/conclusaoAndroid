77e935a9deec8f017097adc23220cbc99eb394b7
Samuel Ribeiro <samuelribeiro9@icloud.com>
2023-04-24 20:10:33 -0300

    dialog implemented

--- app/src/main/java/br/com/conclusaoandroid/AddEditListShoppingDialogFragment.kt
+++ app/src/main/java/br/com/conclusaoandroid/AddEditListShoppingDialogFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.fragment.app.DialogFragment
+import br.com.conclusaoandroid.common.Utils
 import br.com.conclusaoandroid.databinding.DialogfragmentAddEditListShoppingBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -16,6 +17,9 @@ class AddEditListShoppingDialogFragment : DialogFragment() {
         val bundle = requireActivity().intent.extras
         bundle?.getString("documentId").toString()
     }
+    private lateinit var productName: String
+    private lateinit var productValue: String
+    private var productAmount: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,11 +41,43 @@ class AddEditListShoppingDialogFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.dialogTest.setTitleDialog("Olá SAMUEL $documentId")
+        binding.dialogTest.setTitleDialog("Adicionar Produto")
+        binding.dialogTest.setTextFirstField(productName)
+        binding.dialogTest.setTextSecondField(productValue)
         binding.dialogTest.setOnClickButtonPositive {
-            CustomToast.success(requireActivity(), "Ação Dialog")
+            dismiss()
+            addProduct(productValue.toDouble(), productName, productAmount)
+            CustomToast.success(requireActivity(), getString(R.string.registered_successfully))
         }
         binding.dialogTest.setOnClickButtonNegative { dismiss() }
+    }
+
+    private fun addProduct(value: Double, description: String, amount: Int) {
+
+        val purchaseValue = Utils.calcPurchaseValue(amount, value)
+
+        val product = hashMapOf(
+            "description" to description,
+            "value" to value,
+            "amount" to amount,
+            "purchaseValue" to purchaseValue
+        )
+
+        Firebase.firestore
+            .collection("shopping")
+            .document(documentId)
+            .collection("products")
+            .add(product)
+            .addOnSuccessListener {}
+            .addOnFailureListener { e ->
+                CustomToast.error(requireActivity(), "Error")
+            }
+    }
+
+    fun receiveData(productName: String, productValue: String, productAmount: Int) {
+        this.productName = productName
+        this.productValue = productValue
+        this.productAmount = productAmount
     }
 
 }
